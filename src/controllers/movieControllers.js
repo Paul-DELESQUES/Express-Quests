7b9b28a5a9a908f11454a04da38d5adb7f2a0efb
Paul-DELESQUES <pldelesques+github@gmail.com>
2023-11-25 22:30:03 +0100

    update query

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -73,7 +73,7 @@ const getUsersById = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
-    .query("select * from movies where id = ?", [id])
+    .query("select * from users where id = ?", [id])
     .then(([users]) => {
       if (users[0] != null) {
         res.json(users[0]);
